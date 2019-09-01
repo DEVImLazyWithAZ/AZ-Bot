809167c0f9e44804bf2318d486e070381566cbc3
imlazywithazismyname <54771596+imlazywithazismyname@users.noreply.github.com>
2019-09-01 19:37:45 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ async def help(ctx):
     await ctx.send(embed=embed)
     
 @bot.command()
-@commands.has_any_role("lolazimlazy")
+if is_owner()
 async def pingspam(ctx, member : discord.Member= None):
     await ctx.send("{}".format(member.mention))
     await ctx.send("{}".format(member.mention))
