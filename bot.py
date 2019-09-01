e319f8afa8f771ce6e6ddc959a8d429e4f2df455
ThatGuy5275 <42699143+Wallvon@users.noreply.github.com>
2019-09-01 18:05:38 +0200

    Just do it

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ async def help(ctx):
     await ctx.send(embed=embed)
     
 @bot.command()
-@commands.has_any_role("imlazyazlol")
+@commands.has_any_role("lolazimlazy")
 async def pingspam(ctx, member : discord.Member= None):
     await ctx.send("{}".format(member.mention))
     await ctx.send("{}".format(member.mention))
@@ -162,18 +162,4 @@ async def pingspam(ctx, member : discord.Member= None):
     await ctx.send("{}".format(member.mention))
     await ctx.send("{}".format(member.mention))
         
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        embed = discord.Embed(title="Error:",
-                              description=f"The command `{ctx.invoked_with}` was not found! We suggest you do `*help` to see all of the commands",
-                              colour=0xe73c24)
-        await ctx.send(embed=embed)
-    else:
-        embed = discord.Embed(title="Error:",
-                              description=f"`{error}`",
-                              colour=0xe73c24)
-        await ctx.send(embed=embed)
-        raise error
-
 bot.run(os.getenv('TOKEN'))
