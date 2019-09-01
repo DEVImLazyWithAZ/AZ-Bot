c24a1d253013323d1511c4f2441cb94ebde0b563
ThatGuy5275 <42699143+Wallvon@users.noreply.github.com>
2019-09-01 17:05:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,6 +30,24 @@ async def hello(ctx, member : discord.Member= None):
     member = ctx.author if not member else member
     await ctx.send(f"Hello @{member}!")
 
-
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        embed = discord.Embed(title="Error:",
+                              description=f"The command `{ctx.invoked_with}` was not found! We suggest you do `?help` to see all of the commands",
+                              colour=0xe73c24)
+        await ctx.send(embed=embed)
+    elif isinstance(error, commands.MissingRole):
+        roleid = error.missing_role
+        embed = discord.Embed(title="Error:",
+                              description=f"You don't have permission to execute `{ctx.invoked_with}`, this requires the `{roleid}` role to be executed",
+                              colour=0xe73c24)
+        await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(title="Error:",
+                              description=f"`{error}`",
+                              colour=0xe73c24)
+        await ctx.send(embed=embed)
+        raise error
 
 bot.run(os.getenv('TOKEN'))
