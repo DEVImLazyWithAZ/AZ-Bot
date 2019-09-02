029739d1756fbfd5923497cfceb1c57b4d746bb7
imlazywithazismyname <54771596+imlazywithazismyname@users.noreply.github.com>
2019-09-02 18:04:14 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 from discord.ext import commands, tasks
 
 bot = commands.Bot(command_prefix="*")
-bot = commands.Bot(command_prefix="@AZ'S bot")
+
 @bot.event
 async def on_ready():
     print("--------------------")
